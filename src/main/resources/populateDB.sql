afb62d88833c308e63ad0c1202b10598005dca85
Shylo Ihor <ISylo@nvision-group.com>
2017-05-29 12:41:14 +0300

    PopulateDB completed

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -53,14 +53,14 @@ INSERT INTO customers (customer_name, customer_adress, customer_email) VALUES
 ;
 
 INSERT INTO developers_skills (developer_id, skill_id) VALUES
-  (4, 1),
-  (4, 3),
-  (4, 5),
-  (5, 2),
-  (5, 4),
-  (5, 5),
-  (7, 1),
-  (7, 3),
+  (1, 1),
+  (1, 3),
+  (1, 5),
+  (2, 2),
+  (2, 4),
+  (2, 5),
+  (3, 1),
+  (3, 3),
   (6, 1),
   (6, 2),
   (6, 3),
@@ -77,36 +77,56 @@ INSERT INTO developers_skills (developer_id, skill_id) VALUES
   (6, 14),
   (6, 15),
   (6, 16),
-  (8, 1),
-  (8, 3),
-  (8, 5),
-  (8, 7),
-  (8, 9),
-  (8, 11),
-  (8, 13),
-  (8, 15),
-  (9, 2),
-  (9, 4),
-  (9, 6),
-  (9, 8),
-  (9, 10),
-  (9, 12),
-  (9, 14),
-  (9, 16)
+  (4, 1),
+  (4, 3),
+  (4, 5),
+  (4, 7),
+  (4, 9),
+  (4, 11),
+  (4, 13),
+  (4, 15),
+  (5, 2),
+  (5, 4),
+  (5, 6),
+  (5, 8),
+  (5, 10),
+  (5, 12),
+  (5, 14),
+  (5, 16)
 ;
 
 INSERT INTO projects_developers (project_id, developer_id) VALUES
+  (1, 1),
+  (1, 3),
   (1, 4),
   (1, 5),
   (1, 6),
-  (1, 8),
-  (1, 9),
+  (2, 1),
+  (2, 2),
   (2, 4),
-  (2, 5),
   (2, 6),
-  (2, 9),
-  (3, 6),
-  (3, 4),
-  (3, 5),
-  (3, 7)
+  (3, 1),
+  (3, 2),
+  (3, 3),
+  (3, 6)
+;
+
+INSERT INTO companies_projects (companie_id, project_id) VALUES
+  (1, 1),
+  (2, 1),
+  (2, 2),
+  (3, 1),
+  (3, 2),
+  (3, 3),
+  (4, 1),
+  (4, 3)
+;
+
+INSERT INTO customers_projects (customer_id, project_id) VALUES
+  (1, 1),
+  (2, 1),
+  (2, 2),
+  (3, 1),
+  (3, 2),
+  (3, 3)
 ;
